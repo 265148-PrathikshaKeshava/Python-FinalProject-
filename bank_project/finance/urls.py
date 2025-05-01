d93e75ebcf1ddb240f0fd8f35f9af3d2081207b1
Lakith kumar GK <264933@ust.com>
2025-05-01 11:13:32 +0530

    done

--- bank_project/finance/urls.py
+++ bank_project/finance/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.login_view, name='login'),
+    path('login/', views.login_view, name='login'),  # Correct path for login
     path('signup/', views.signup_view, name='signup'),
     path('logout/', views.logout_view, name='logout'),
-    path('home/', views.emi_calculator, name='home'),
+    path('home/', views.emi_calculator, name='home'),  # Home page should be accessible at /home/
     path('sip/', views.sip_calculator, name='sip_calculator'),
     path('fd/', views.fd_calculator, name='fd_calculator'),
     path('rd/', views.rd_calculator, name='rd_calculator'),
